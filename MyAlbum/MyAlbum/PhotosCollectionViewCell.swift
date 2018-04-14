9779d7ced06f7662ba4804d87269981a3b1d3ab1
clapwatermelon <clapwatermelon@gmail.com>
2018-04-14 23:21:00 +0900

    practice Photos framework

---
+++ MyAlbum/MyAlbum/PhotosCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  PhotosCollectionViewCell.swift
+//  MyAlbum
+//
+//  Created by 박수현 on 14/04/2018.
+//  Copyright © 2018 박수현. All rights reserved.
+//
+
+import UIKit
+
+class PhotosCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var photoCount: UILabel!
+    @IBOutlet weak var albumTitle: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+}
